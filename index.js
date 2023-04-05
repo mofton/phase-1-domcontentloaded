365d949b66e8411784c440760dd93914eb708279
Mofeid Moshreky <mof_ton@yahoo.com>
2023-04-05 05:20:24 -0400

    compleate lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let p = document.querySelector("p");
+p.textContent =
+  "JavaScript is so cool. It lets me add text to my page programmatically.";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let p = document.querySelector("p");
+  p.textContent = "This is really cool!";
+});
